1c9b6406e44c23e5260e68123eb54315bfef503e
Philip Zhu <62172698+philipzhux@users.noreply.github.com>
2021-08-13 18:51:18 +0800

    Time: 80 ms (52.58%), Space: 42.2 MB (70.93%) - LeetHub

---
+++ maximum-binary-tree/maximum-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <algorithm>
+#include <bits/stdc++.h>
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return build(nums,0,nums.size()-1);
+        
+    }
+    
+    TreeNode* build(const vector<int>& nums,int left,int right) {
+        int max = INT_MIN;
+        int index;
+        for(int i=left;i<=right;i++){
+            if(nums[i]>max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+        if(left>right) return NULL;
+        if(left==right) return (new TreeNode(nums[left]));
+        TreeNode* root = new TreeNode(max,build(nums,left,index-1),build(nums,index+1,right));
+        return root;
+    }
+};
