3c9947b65adfe1fad31bbc9674cab5a17e72a42d
Philip Zhu <62172698+philipzhux@users.noreply.github.com>
2021-08-14 16:11:35 +0800

    Time: 48 ms (13.18%), Space: 33.6 MB (20.40%) - LeetHub

--- convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
+++ convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -12,16 +12,30 @@
 class Solution {
 public:
     TreeNode* convertBST(TreeNode* root) {
-        traverse(root);
+        //traverse(root);
+        int sum = 0;
+        stack<TreeNode*> des;
+        TreeNode* ptr = root;
+        while(!des.empty()||ptr){
+            while(ptr){
+                des.push(ptr);
+                ptr = ptr->right;
+            }
+            ptr = des.top();
+            des.pop();
+            sum+= ptr->val;
+            ptr->val = sum;
+            ptr = ptr->left;
+        }
         return root;
     }
 private:
-    int sum=0;
-    void traverse(TreeNode* root) {
-        if(!root) return;
-        traverse(root->right);
-        sum += root->val;
-        root->val = sum;
-        traverse(root->left);
-    }
+    // int sum=0;
+    // void traverse(TreeNode* root) {
+    //     if(!root) return;
+    //     traverse(root->right);
+    //     sum += root->val;
+    //     root->val = sum;
+    //     traverse(root->left);
+    //}
 };
