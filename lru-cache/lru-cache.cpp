39e23425d3336776f15da2d8aedcf05053e5e7a2
Philip Zhu <62172698+philipzhux@users.noreply.github.com>
2021-08-15 19:29:06 +0800

    Time: 412 ms (66.30%), Space: 174.1 MB (48.39%) - LeetHub

--- lru-cache/lru-cache.cpp
+++ lru-cache/lru-cache.cpp
@@ -5,43 +5,39 @@ public:
     }
     
     int get(int key) {
-        if(!hash.count(key)) return -1;
+        if(!hash_list.count(key)) return -1;
         //marked as most recently used
+        int value = hash_list[key]->second;
         cache.erase(hash_list[key]);
-        cache.push_back(key);
+        cache.push_back(make_pair(key,value));
         hash_list[key] = prev(cache.end());
         //return query result
-        return hash[key];
+        return value;
     }
     
     void put(int key, int value) {
-        if(hash.count(key)) {
-            //marked as most recently used
+        if(hash_list.count(key)) {
+            //marked as most recently used and update value
             cache.erase(hash_list[key]);
-            cache.push_back(key);
+            cache.push_back(make_pair(key,value));
             hash_list[key] = prev(cache.end());
-            //update value
-            hash[key]=value;
             return;
         }
         if(count==capacity) {
-            int rm = cache.front();
+            hash_list.erase(cache.front().first);
             cache.pop_front();
-            hash.erase(rm);
-            hash_list.erase(rm);
             count--;
         }
         count++;
-        cache.push_back(key);
-        hash[key] = value;
+        cache.push_back(make_pair(key,value));
         hash_list[key] = prev(cache.end());
     }
 private:
     int count;
     int capacity;
-    list<int> cache;
-    unordered_map<int,int> hash;
-    unordered_map<int,list<int>::iterator> hash_list;
+    list<pair<int,int>> cache;
+    //unordered_map<int,int> hash;
+    unordered_map<int, list<pair<int,int>>::iterator> hash_list;
 };
 
 /**
