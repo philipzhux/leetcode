a28f07783c38477358553d672968e14e45cc9031
Philip Zhu <62172698+philipzhux@users.noreply.github.com>
2022-02-21 11:34:09 -0800

    Time: 65 ms (91.65%), Space: 19.8 MB (99.92%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size()<3) return res;
+        sort(nums.begin(),nums.end());
+        auto last = nums.end();
+        int target = 0;
+        for(auto i=nums.begin();i<last-2;i++) {
+            if (i > nums.begin() && *i == *(i-1)) continue;
+            auto j = i+1;
+            auto k = last-1;
+            while(j<k) {
+                if((*i+*j+*k)<target) {
+                    j++;
+                     while(*j==*(j-1) && j<k) j++;
+                    
+                }
+                else if((*i+*j+*k)>target) {
+                    k--;
+                    while(*k==*(k+1) && j<k) k--;
+                }
+                else {
+                    res.push_back({*i,*j,*k});
+                    j++;
+                    k--;
+                    while(*j == *(j - 1) && *k == *(k + 1) && j < k) ++j;
+                }
+            }
+        }
+        return res;
+    }
+};
