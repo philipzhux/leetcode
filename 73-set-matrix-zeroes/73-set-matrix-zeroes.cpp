88c5bff42ed86501c3c53f947e153ea932d30055
Philip Zhu <62172698+philipzhux@users.noreply.github.com>
2022-02-23 23:44:23 -0800

    Time: 23 ms (42.31%), Space: 13.3 MB (19.79%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int> z_rows, z_cols;
+        int n = matrix.size();
+        if(!n) return;
+        int m = matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0)
+                {
+                    z_cols.insert(j);
+                    z_rows.insert(i);
+                }
+            }
+        }
+        for(const auto &t: z_rows){
+            for(int i=0;i<m; i++) matrix[t][i] = 0;
+        }
+        
+        for(const auto &t: z_cols){
+            for(int i=0;i<n; i++) matrix[i][t] = 0;
+        }
+    }
+};
